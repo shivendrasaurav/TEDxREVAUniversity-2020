1a58ef0e2777e9737b08cb0e2926e2b3f448aa56
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-03-20 22:57:49 +0530

    Added Speakers Info Javascript

---
+++ speakersinfo.js
@@ -0,0 +1,69 @@
+function openmodal1(){
+    document.getElementById("mod1").style.display=("block");
+        }
+function closemodal1(){
+    document.getElementById("mod1").style.display=("none");
+}
+
+function openmodal2(){
+    document.getElementById("mod2").style.display=("block");
+        }
+function closemodal2(){
+    document.getElementById("mod2").style.display=("none");
+}
+
+function openmodal3(){
+    document.getElementById("mod3").style.display=("block");
+        }
+function closemodal3(){
+    document.getElementById("mod3").style.display=("none");
+}
+
+function openmodal4(){
+    document.getElementById("mod4").style.display=("block");
+        }
+function closemodal4(){
+    document.getElementById("mod4").style.display=("none");
+}
+
+function openmodal5(){
+    document.getElementById("mod5").style.display=("block");
+        }
+function closemodal5(){
+    document.getElementById("mod5").style.display=("none");
+}
+
+function openmodal6(){
+    document.getElementById("mod6").style.display=("block");
+        }
+function closemodal6(){
+    document.getElementById("mod6").style.display=("none");
+}
+
+function openmodal7(){
+    document.getElementById("mod7").style.display=("block");
+        }
+function closemodal7(){
+    document.getElementById("mod7").style.display=("none");
+}
+
+function openmodal8(){
+    document.getElementById("mod8").style.display=("block");
+        }
+function closemodal8(){
+    document.getElementById("mod8").style.display=("none");
+}
+
+function openmodal9(){
+    document.getElementById("mod9").style.display=("block");
+        }
+function closemodal9(){
+    document.getElementById("mod9").style.display=("none");
+}
+
+function openmodal10(){
+    document.getElementById("mod10").style.display=("block");
+        }
+function closemodal10(){
+    document.getElementById("mod10").style.display=("none");
+}
